27ee3dd3829ec1cb285662f39c882388512c3951
nullppon <nullpon16tera@github.com>
2022-02-22 01:40:03 +0900

    ふぁずぱいさん作成のRundomize Arrows追加と細かな調整

---
+++ NoteMode/HarmonyPatches/BeatmapObjectsInTimeRowProcessor.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using NoteMode.Configuration;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace NoteMode.HarmonyPatches
+{
+    [HarmonyPatch(typeof(BeatmapObjectsInTimeRowProcessor), "ProcessAllNotesInTimeRow")]
+    static class BeatmapObjectsInTimeRowProcessorProcessAllNotesInTimeRow
+    {
+        static void Prefix(List<NoteData> notesInTimeRow)
+        {
+            if (notesInTimeRow.Count == 0)
+            {
+                return;
+            }
+
+            for (int j = 0; j < notesInTimeRow.Count; j++)
+            {
+                NoteData noteData = notesInTimeRow[j];
+                Plugin.Log.Debug($"{noteData.lineIndex}");
+            }
+            notesInTimeRow.Clear();
+        }
+    }
+}

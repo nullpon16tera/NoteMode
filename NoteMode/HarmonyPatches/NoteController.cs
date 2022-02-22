e8eaceada2cf9891fe96f12e435d16ef0b397800
nullppon <nullpon16tera@github.com>
2022-02-22 22:26:31 +0900

    ノーツの位置をテストで変更してみた。

--- NoteMode/HarmonyPatches/NoteController.cs
+++ NoteMode/HarmonyPatches/NoteController.cs
@@ -9,14 +9,26 @@ namespace NoteMode.HarmonyPatches
     [HarmonyPatch(typeof(NoteController), "Init")]
     static class NoteControllerInit
     {
-        static void Postfix(Transform ____noteTransform)
+        static void Postfix(ref NoteData noteData, Transform ____noteTransform)
         {
-
             if (NoteModeController.instance.inGame == true)
             {
                 if (PluginConfig.Instance.isNotesScale)
                 {
-                    ____noteTransform.gameObject.transform.localScale = Vector3.one * PluginConfig.Instance.notesScale;
+                    float getScale = PluginConfig.Instance.notesScale;
+                    float x = 0f;
+                    float y = 0f;
+                    if (noteData.lineIndex == 0)
+                    {
+                        x = 1f - getScale + 0.1f;
+                    }
+                    if (noteData.lineIndex == 3)
+                    {
+                        x = -(1f - getScale + 0.1f);
+                    }
+
+                    ____noteTransform.gameObject.transform.localScale = Vector3.one * getScale;
+                    ____noteTransform.gameObject.transform.localPosition = new Vector3(x, 0f, -(1f - getScale));
                 }
             }
         }
